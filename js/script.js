20858661284520925f5d997b730f29ff0d194dba
MattiaManfreda <mattiamanfreda926@gmail.com>
2021-04-08 17:38:07 +0200

    freccia-destra-finita

--- js/script.js
+++ js/script.js
@@ -1,17 +1,31 @@
+
+// Freccia Destra
+var rightArrow = $('.right-arrow');
+
+$(rightArrow).click(function(){
+    var currentActive = $('.active');
+    currentActive.removeClass('active');
+
+    if ( currentActive.hasClass('last') ) {
+        //Riparto dalla prima immagine
+        $('.images .first').addClass('active');
+    } else {
+        //seleziono il fratello successivo
+        var nextImg = currentActive.next();
+        console.log(nextImg);
+
+        //aggiungo la classe active al successivo
+        nextImg.addClass('active');
+    }
+
+});
+
+
+
 // Freccia Sinistra
 var leftArrow = $('.left-arrow');
 
 $(leftArrow).click(function(){
 
     alert("accesso alla funzione");
-});
-
-
-
-// Freccia Destra
-var rightArrow = $('.right-arrow');
-
-$(rightArrow).click(function(){
-
-    
 });
