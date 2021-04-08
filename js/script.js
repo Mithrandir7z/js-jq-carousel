5ce3b29edf9cc7ee2f2ee6d5a4ce75dec0f4a03c
MattiaManfreda <mattiamanfreda926@gmail.com>
2021-04-08 16:59:01 +0200

    sviluppo ulteriore

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Freccia Sinistra
+var leftArrow = $('.left-arrow');
+
+$(leftArrow).click(function(){
+
+    alert("accesso alla funzione");
+});
+
+
+
+// Freccia Destra
+var rightArrow = $('.right-arrow');
+
+$(rightArrow).click(function(){
+
+    
+});
